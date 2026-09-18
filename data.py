545891fbaa44f6b90e58a4680db6c0baebb1ae9d
avinl2 <avinl2@nycstudents.net>
2026-09-18 09:20:13 -0400

    unit 2

---
+++ data.py
@@ -0,0 +1,3 @@
+sentence = input ("Please input a sentence")
+word_count = len(sentence.split())
+print(word_count)
---
+++ test.py
@@ -0,0 +1,22 @@
+#String for characters
+name = "Inaya"
+# #input asks the user a question and records the answer
+#what we write in input argument is what user sees 
+#input ALWAYS outputs a string
+""" bill = int(input("How much was the bill"))
+print(bill)  """
+
+""" if bill == 10:
+    print("Match")
+else:
+    print("no match") """
+
+""" #integer for whole number
+amt = 100
+#Float uses decimal
+amt_two = 99.99 """
+
+#Boolean
+""" x = True
+y = False
+ """

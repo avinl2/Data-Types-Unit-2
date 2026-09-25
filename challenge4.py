09439b95fd2314bbcf89362fe432a6139952948f
avinl2 <avinl2@nycstudents.net>
2026-09-25 09:21:37 -0400

    Update challenge4.py

--- challenge4.py
+++ challenge4.py
@@ -1,5 +1,5 @@
 number = int(input("Enter the first number."))
-value = int(input("Enter the second number."))
+number2 = int(input("Enter the second number."))
 
 factor = []
 factor2 = []
@@ -10,7 +10,15 @@ for i in range(1, number + 1):
         factor.append(i)
 
 
-for i in range(1, value + 1):
+for i in range(1, number2 + 1):
     if number % i ==0:
         factor2.append(i)
 
+x = 0 
+for i in range(len(factor)):
+    for i in range(len(factor2)):
+        if factor[x] == factor2[i]:
+            gcf.append(factor2[x])
+    x+=1
+
+print(f"The greatest common factor is {gcf[-1]}.")
